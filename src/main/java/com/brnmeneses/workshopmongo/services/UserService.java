d3f52e1cfcb0376cf8a7439e000d20a7c6c514d0
Brnmeneses <brnmeneses@gmail.com>
2019-02-15 19:49:05 -0200

    DELETE

--- src/main/java/com/brnmeneses/workshopmongo/services/UserService.java
+++ src/main/java/com/brnmeneses/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.brnmeneses.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brnmeneses.workshopmongo.domain.User;
+import com.brnmeneses.workshopmongo.dto.UserDTO;
 import com.brnmeneses.workshopmongo.repository.UserRepository;
+import com.brnmeneses.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +20,24 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		repo.findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+
 }
